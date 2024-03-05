20944fd61979ed9b75d43d148d6fda91581d3c92
liam <wingstedt.liam@gmail.com>
2024-03-05 09:36:10 +0100

    uj

---
+++ +page.svelte
@@ -0,0 +1,29 @@
+<script>
+let antallOppgradering = 0
+let poeng = 0
+let test = 0
+function faaPoeng(){
+poeng = poeng + (1 + antallOppgradering)
+}
+function oppgradering(){
+    poeng = poeng - 100
+    antallOppgradering = antallOppgradering +1 
+
+}
+
+
+</script>
+
+<button on:click={faaPoeng}>Trykk</button>
+<h1>Kjeks Clicker</h1>
+<button on:click={oppgradering}>Kjøp oppgradering</button>
+<p>{poeng}</p>
+
+
+<style>
+
+
+
+</style>
+
+<p>hei</p>
---
+++ src/routes/memorycard/+page.svelte
@@ -0,0 +1,93 @@
+<script>
+
+
+</script>
+
+<!--Main div for alle kortene-->
+<div class="main">
+    <ul class="kort1">
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+        <li class="kort2">
+            <div class="front"></div>
+            <div class="back"></div>
+        </li>
+
+
+    </ul>
+
+</div>
